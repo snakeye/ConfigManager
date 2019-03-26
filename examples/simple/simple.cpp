9b7270da8a2349033951e855e0bc2f5c4a0a7669
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2019-03-26 19:58:53 +0100

    Updated documentation, removed obsolete examples

---
+++ examples/simple/simple.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+#include <ConfigManager.h>
+
+struct Config
+{
+    char name[20] = {0};
+    bool enabled = false;
+    int hour = 0;
+} config;
+
+ConfigManager configManager;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    configManager.setAPName("Config Demo");
+    configManager.setAPFilename("/index.html");
+
+    configManager.addParameterGroup("app", new Metadata("Application", "Example of application properties"))
+        .addParameter("name", config.name, 20, new Metadata("Name"))
+        .addParameter("enabled", &config.enabled, new Metadata("Enabled"))
+        .addParameter("hour", &config.hour, new Metadata("Hour"));
+
+    configManager.begin(config);
+}
+
+void loop()
+{
+    configManager.loop();
+
+    delay(1000);
+}
